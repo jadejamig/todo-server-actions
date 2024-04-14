4938a119c19f3b637390c3bcc9ea442a0c60b397
Anselm Jade Jamig <jadejamigdev@gmail.com>
2024-04-15 00:59:10 +0800

    transitioned from postgres to vercel postgres

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7643e7b400677a5e"; }
+/******/ 		__webpack_require__.h = function() { return "fac9afce338b43df"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
