bc086450deff2fc6de1b4ba3dd5733850b949c4b
Anselm Jade Jamig <jadejamigdev@gmail.com>
2024-04-15 17:09:08 +0800

    vercel postgres to supabase

--- app/add-form.tsx
+++ app/add-form.tsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import prisma from './db'
-import { revalidatePath } from 'next/cache'
-import FormButton from './FormButton'
+import { revalidatePath } from 'next/cache';
+import prisma from './db';
+import FormButton from './FormButton';
 
 const AddForm = () => {
 
-    
-      
     async function createTodo(data: FormData) {
         "use server"
-
+    
         const title = data.get('title')?.valueOf()
         if (typeof title !== 'string' || title.length === 0)
             return
-
+    
         await prisma.todo.create({
             data: {title}
         })
